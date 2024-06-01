fc77f3d94b5e5463941d239509f9e5a15479e152
Jamie White <133122446+jdrgw@users.noreply.github.com>
2024-06-01 19:16:02 +0100

    Created url path and html page with dummy text for mp data
    ToDo: model members and display on members-of-parliament with pagination

--- members_interest_app/views.py
+++ members_interest_app/views.py
@@ -8,3 +8,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'members_interest_app/index.html')
+def members_of_parliament(request):
+    return render(request, 'members_interest_app/members-of-parliament.html')
