a560c718bf28e486c3cca52dae09dea564240833
Jamie White <133122446+jdrgw@users.noreply.github.com>
2024-06-01 14:57:35 +0100

    Create basic index path and landing page with bootstrap.
    TODO: url links to other parts of the site once those paths have been created.

--- parliament_data/urls.py
+++ parliament_data/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+	path('', include("members_interest_app.urls")),
+	path('mp-data/', include("members_interest_app.urls")),
+	path('home/', include("members_interest_app.urls")),
     path('admin/', admin.site.urls),
 ]
