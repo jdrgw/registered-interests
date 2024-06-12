c205fb7567eae17869142d4ed5b123ee59647d3a
Jamie White <133122446+jdrgw@users.noreply.github.com>
2024-06-12 15:36:17 +0100

    Create path, page and test for displaying individual MP data on website

--- members_interest_app/urls.py
+++ members_interest_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index-page'),
     path('members-of-parliament/', views.members_of_parliament, name='members-of-parliament'),
+    path('member/<int:pk>/', views.member, name='member'),
 ]
