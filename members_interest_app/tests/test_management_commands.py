742a59e5eaad014d1b4432733cae59f36efc599d
Jamie White <133122446+jdrgw@users.noreply.github.com>
2024-08-07 10:29:58 +0100

    Create management command and test for running call_members_api util function

--- members_interest_app/tests/test_management_commands.py
+++ members_interest_app/tests/test_management_commands.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from io import StringIO
 from django.core.management import call_command
+from unittest.mock import patch
 
 class TestRunUnpackSaveMembersData(TestCase):
     def test_management_command_output(self):
@@ -9,3 +10,15 @@ class TestRunUnpackSaveMembersData(TestCase):
         result = out.getvalue()
         expected_output_slice = "Total members added:"
         self.assertIn(expected_output_slice, result)
+
+class TestCallMembersAPI(TestCase):
+
+    @patch('members_interest_app.utils.call_members_api.fetch_members_data')
+    def test_management_command_output(self, mock_fetch_members_data):
+        # Setup the mock return value
+        mock_fetch_members_data.return_value = "Mocked fetch_members_data function called"
+        out = StringIO()
+        call_command("call_members_api", stdout=out)
+        self.assertIn("Mocked fetch_members_data function called", out.getvalue())
+
+        mock_fetch_members_data.assert_called_once()
