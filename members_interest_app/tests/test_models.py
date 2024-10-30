29137b80ec6414ceb909c8ce7884649c83b90e43
Jamie White <133122446+jdrgw@users.noreply.github.com>
2024-10-30 11:24:08 +0000

    Implement test for new RegisteredInterest model

--- members_interest_app/tests/test_models.py
+++ members_interest_app/tests/test_models.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from django.utils import timezone
 from members_interest_app.models import (
     House,
     MemberOfParliament,
+    RegisteredInterest,
 )
 
 
@@ -80,3 +83,103 @@ class TestHouse(TestCase):
             # Create a House with name=None, which should raise a ValidationError
             house = House(name=None)
             house.full_clean()
+
+
+class RegisteredInterestModelTests(TestCase):
+
+    def setUp(self):
+        self.mp = MemberOfParliament.objects.create(api_id='123', name='Ron Swanson')
+
+    def test_save_registered_interest_with_valid_mp(self):
+        registered_interest = RegisteredInterest(
+        member_of_parliament=self.mp, 
+        api_id='interest_001',
+        unique_api_generated_id='unique_id_001', 
+        category_id='category_001',
+        category_name='Category Name',
+        sort_order='1',
+        interest_summary='Summary of interest',
+        date_created=timezone.now(),
+        date_last_amended=timezone.now(),
+        date_deleted=None,  
+        is_correction=False,
+        is_child_interest=False,
+        parent_interest="1", 
+        interest_amount=100.00,
+        interest_currency='GBP',
+        number_of_extracted_amounts=None,
+        contains_loan=False,
+        contains_time_period=None,
+        payer='John Smith',
+        payer_type='person',
+        payer_address=None,
+        payer_companies_house_id=None,
+        purpose='Purpose of the interest',
+        role='Role in interest',
+        employer_name='Pawnee',
+        family_member_name=None,
+        family_member_relationship=None,
+        family_member_role=None,
+        family_member_paid_by_mp_or_parliament=None,
+        family_member_lobbies=True
+        )
+        
+        try:
+            registered_interest.save()
+        except ValidationError:
+            self.fail("RegisteredInterest.save() raised ValidationError unexpectedly!")
+
+        self.assertEqual(RegisteredInterest.objects.count(), 1)
+
+
+    def test_save_registered_interest_with_invalid_mp(self):
+        registered_interest = RegisteredInterest(
+            member_of_parliament=None,  # Invalid member_of_parliament
+            api_id='interest_002',
+            unique_api_generated_id='unique_id_002',
+            category_id='category_002',
+            category_name='Category Name 2',
+            sort_order='2',
+            interest_summary='Summary of interest 2',
+            date_created=timezone.now(),
+            date_last_amended=timezone.now(),
+            date_deleted=None,
+            is_correction=False,
+            is_child_interest=False,
+            parent_interest="1", 
+            interest_amount=None,
+            interest_currency=None,
+            number_of_extracted_amounts=None,
+            contains_loan=None,
+            contains_time_period=None,
+            payer=None,
+            payer_type=None,
+            payer_address=None,
+            payer_companies_house_id=None,
+            purpose=None,
+            role='Role in interest',
+            employer_name='Pawnee',
+            family_member_name=None,
+            family_member_relationship=None,
+            family_member_role=None,
+            family_member_paid_by_mp_or_parliament=None,
+            family_member_lobbies=True
+        )
+        
+        with self.assertRaises(IntegrityError):
+            registered_interest.save()
+
+
+    def test_string_representation(self):
+        registered_interest = RegisteredInterest(
+            member_of_parliament=self.mp,
+            api_id='interest_003',
+            category_id='category_003',
+            category_name='Category Name 3',
+            sort_order='3',
+            interest_summary='Another summary of interest',
+            date_created=timezone.now(),
+            date_last_amended=timezone.now(),
+        )
+        
+        self.assertEqual(str(registered_interest), 'Category Name 3 - Another summary of interest')
